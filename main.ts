9ab83cd7c759b4d9700ad9baf6db3cfd27f233c7
zahida-abbasi <145322592+zahida-abbasi@users.noreply.github.com>
2024-04-19 15:19:50 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+async function main() {
+    let todos = [];
+    let condition = true;
+
+    while (condition) {
+        let addTask = await inquirer.prompt([
+            {
+                name: "todo",
+                type: "input",
+                message: "What do you want to add to your todo list?"
+            },
+            {
+                name: "addAnother",
+                type: "confirm",
+                message: "Do you want to add more?",
+                default: "false"
+            }
+        ]);
+
+        todos.push(addTask.todo);
+        condition = addTask.addAnother;
+    }
+
+    console.log(todos);
+}
+
+main();
